c99d889909aa6e7246de0f30cef6df325bf47501
Paul Rey <paul.rey@y-nov.com>
2016-03-10 15:54:48 +0100

    push du morceau

--- src/com/ynov/transpotter/controllers/LoginController.java
+++ src/com/ynov/transpotter/controllers/LoginController.java
@@ -10,10 +10,17 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class LoginController {
     private final static Logger logger = Logger.getLogger(LoginController.class.getName());
-
+    
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String getLogin(Model model) {
 	logger.info("getLogin");
+	return "login";
+    }
+    
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String postLogin(Model model) {
+	logger.info("getLogin");
+	System.out.println("toto");
 	return "login";
     }
 
